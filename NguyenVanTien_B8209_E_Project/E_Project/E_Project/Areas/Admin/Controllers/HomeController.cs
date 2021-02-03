4e6ef8dc3ac51d1622db74368f2b2b539fa57091
OP-2VYMH33\tiennv <tiennv@onepay.vn>
2021-02-03 21:09:29 +0700

    update 03/02/2021

---
+++ NguyenVanTien_B8209_E_Project/E_Project/E_Project/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using E_Project.Areas.Admin.Model;
+using E_Project.Areas.Admin.Utils;
+using ModelEntity;
+using ModelEntity.Frameworks;
+
+namespace E_Project.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            InfoSession modelSession = new InfoSession();
+
+            modelSession = SessionHelper.GetInfoSession();
+            Session["infoSession"] = modelSession;
+            return View();
+        }
+    }
+}
--- NguyenVanTien_B8209_E_Project/E_Project/E_Project/Areas/Admin/Utils/InfoSession.cs
+++ NguyenVanTien_B8209_E_Project/E_Project/E_Project/Areas/Admin/Utils/InfoSession.cs
@@ -10,6 +10,7 @@ namespace E_Project.Areas.Admin.Utils
     {
         public int id { get; set; }
         public string account { get; set; }
+
         public string password { get; set; }
         public string phone { get; set; }
         public string fullName { get; set; }
